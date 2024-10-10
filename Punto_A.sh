fae3f7ee569702b0050922cb0d7b614a1a3fc0ed
JulianJm04 <1julian.maldonado.jm@gmail.com>
2024-10-10 17:28:14 +0000

    Eliminado Punto_A.sh para volver a agregar

--- Punto_A.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Crear la estructura de directorios con alumnos y profesores
-sudo mkdir -p /Examenes-UTN/{alumno_1/alumno_1/parcial_{1,2,3},alumno_2/alumno_2/parcial_{1,2,3},alumno_3/alumno_3/parcial_{1,2,3},profesores}
-
