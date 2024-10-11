12d168db4fc3e149b9c1d903edd4077d0afc4238
JulianJm04 <1julian.maldonado.jm@gmail.com>
2024-10-11 15:39:18 +0000

    Eliminados scripts duplicados

--- Punto_D.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Creamos la estructura en el home
-mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},clientes/cartas_{1..100}}
-
-# Verificaion de la creacion de la estructura
-tree ~/Estructura_Asimetrica/ --noreport | pr -T -s'' -w 80 --column 4
