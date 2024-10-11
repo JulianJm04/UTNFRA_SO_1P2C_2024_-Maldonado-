85f52417d8fcbb344fad11c89d5884ea44f6f1cf
JulianJm04 <1julian.maldonado.jm@gmail.com>
2024-10-11 15:28:42 +0000

    Estructura de carpetas y archivos organizada para el examen

---
+++ RTA_SCRIPT_Examen20241011/Punto_A.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Crear la estructura de directorios con alumnos y profesores
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
---
+++ RTA_SCRIPT_Examen20241011/Punto_E.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Directorio en el que se va a aguardar el archivo
+DIRECTORIO=~/repogit/UTNFRA_SO_1P2C_2024_-Maldonado-/RTA_ARCHIVOS_Examen_$(date +%Y%m%d)
+
+# Crear directorio si no existe
+mkdir -p $DIRECTORIO
+
+# Extraer la memoria total
+memoria=$(grep MemTotal /proc/meminfo | awk -F ':' '{print$2 " " $3}')
+
+# Extraer la info del chasis
+chasis=$(sudo dmidecode -t chassis | grep "Manufacturer" | awk -F ':' '{print$2}')
+
+# Guardo la info en el archivo
+echo "MemTotal: $memoria" > $DIRECTORIO/Filtro_Basico.txt
+echo "Chasis: $chasis" >> $DIRECTORIO/Filtro_Basico.txt
+
+echo "El archivo FiltroBasico se ha creado en el directorio"
+cat $DIRECTORIO/Filtro_Basico.txt
